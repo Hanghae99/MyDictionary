ea4357e92bc27f83506408d1b6adae691c9941ea
YeonJae <duswo9595@naver.com>
2022-03-25 15:06:48 +0900

    feat:header 작업완료

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header/header";
+import MainContents from "./components/mainContents/mainContents";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContents />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = (props) => {
+  return (
+    <div className="top-border">
+      <h1 className="title-name">My Dictionary :)</h1>
+    </div>
+  );
+};
+
+export default Header;
